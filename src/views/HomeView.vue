34e17c46f931af0722c39f5436c867bb75a1f92e
lizhiquan <842048365@qq.com>
2023-03-05 14:54:37 +0800

    feat: 目录清理

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,7 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <div class="home-container">home</div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+<script setup lang="ts"></script>
 
-export default defineComponent({
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
-});
-</script>
+<style lang="scss" scoped></style>
