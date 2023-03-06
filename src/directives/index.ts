cf46ae2d2cc51e2f1ba9ce60fd98155335e3ec48
lizhiquan <842048365@qq.com>
2023-03-06 21:51:25 +0800

    perf: 优化directive代码

--- src/directives/index.ts
+++ src/directives/index.ts
@@ -4,15 +4,13 @@ export interface IDrrective {
   directive: any;
 }
 
-const directiveList: Array<IDrrective> = [];
-
-const files = require.context(`./modules`, true, /\.ts$/);
-files.keys().forEach((key) => {
-  const file = require(`./modules` + key.split('.')[1]).default;
-  directiveList.push(file);
-});
-
 export default function directives(app: App): void {
+  const directiveList: Array<IDrrective> = [];
+  const files = require.context(`./modules`, true, /\.ts$/);
+  files.keys().forEach((key) => {
+    const file = require(`./modules` + key.split('.')[1]).default;
+    directiveList.push(file);
+  });
   for (const direct of directiveList) {
     app.directive(direct.name, direct.directive);
   }
